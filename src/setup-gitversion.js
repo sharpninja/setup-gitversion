61d9a9e851a9e97706f126510e707155ab7ab7d3
The Sharp Ninja <ninja@thesharp.ninja>
2022-06-08 12:50:49 -0500

    Update setup-gitversion.js

--- src/setup-gitversion.js
+++ src/setup-gitversion.js
@@ -27,7 +27,7 @@ async function main() {
   await installGitversion(gitversionVersion)
   console.log(`##[endgroup]`)
 
-  if (runGitversion) await exec('GitVersion', runGitversionArgs)
+  if (runGitversion) await exec('gitversion', runGitversionArgs)
 }
 
 function checkPlatform() {
